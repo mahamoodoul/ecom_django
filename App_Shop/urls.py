01e48c10d30acfb35b84665a9a673e0d300e3d5b
shakil <mahamodul.shakil11@gmail.com>
2021-01-17 20:25:47 +0600

    first commit

---
+++ App_Shop/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+
+app_name = 'App_Shop'
+
+urlpatterns = [
+    # path('', views.Home.as_view(), name='home'),
+    # path('product/<pk>/', views.ProductDetail.as_view(), name='product_detail'),
+]
